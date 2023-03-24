1731b333bf030a621b5d5f137348e0a26332c652
ticoniq_ <okolietochi@gmail>
2023-03-24 21:12:05 +0100

    Delete and clear items from storage

--- app.js
+++ app.js
@@ -30,6 +30,29 @@ const StorageCtrl = (function() {
         items = JSON.parse(localStorage.getItem('items'));
       }
       return items;
+    }, 
+    updateItemStorage: function(updatedItem) {
+      let items = JSON.parse(localStorage.getItem('items'));
+      items.forEach(function(item, index){
+        if (updatedItem.id === item.id) {
+          items.splice(index, 1, updatedItem);
+        }
+      });
+      // set localStorage 
+      localStorage.setItem('items', JSON.stringify(items));
+    }, 
+    deleteItemFromStorage: function(id) {
+      let items = JSON.parse(localStorage.getItem('items'));
+      items.forEach(function(item, index){
+        if (id === item.id) {
+          items.splice(index, 1);
+        }
+      });
+      // set localStorage 
+      localStorage.setItem('items', JSON.stringify(items));
+    },
+    clearItemsFromStorage: function() {
+      localStorage.removeItem('items');
     }
   }
 })();
@@ -377,6 +400,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl){
      // Add total calories to UI
      UICtrl.showTotalCalories(totalCalories);
 
+    //  Update localStorage 
+    StorageCtrl.updateItemStorage(updatedItem);
+
      UICtrl.clearEditState();
 
     e.preventDefault();
@@ -398,6 +424,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl){
     // Add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
 
+    // delete from localStorage
+    StorageCtrl.deleteItemFromStorage(currentItem.id);
+    
     UICtrl.clearEditState();
 
     e.preventDefault();
@@ -415,6 +444,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl){
 
     // remove from ui controller 
     UICtrl.removeItems();
+
+    // clear from localstorage 
+    StorageCtrl.clearItemsFromStorage();
 
     // Hide the list 
     UICtrl.hideList();
